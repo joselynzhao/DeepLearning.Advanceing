21d1f47fd447f4767cd74f3bfd0481bdd78288f7
zhaojing1995 <1326549593@qq.com>
2019-05-05 12:00:29 +0800

    init

---
+++ DL-2/graph.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn
+@CONTACT:zhaojing17@foxmail.com
+@SOFTWERE:PyCharm
+@FILE:graph.py
+@TIME:2019/5/5 11:56
+@DES:
+'''
+
+if __name__ =="__main__":
+    import  torch
+    v1 = torch.rand(1,3)
+    v2 = torch.rand(1,3)
+    sum = v1+v2
+    print(v1)
+    print(v2)
+    print(sum)
---
+++ DL-2/practice1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn
+@CONTACT:zhaojing17@foxmail.com
+@SOFTWERE:PyCharm
+@FILE:practice1.py
+@TIME:2019/5/5 11:38
+@DES:
+'''
+
+import  tensorflow as tf
+import torch
+
+
+
+if __name__ == "__main__":
+
+    def graph_a():
+        a = tf.constant(2, name='a')
+        # print(a.name,a)
+
+        b = tf.constant(3, name='b')
+        # print(b.name, b)
+
+        result = tf.add(a,b,name = 'add')
+
+
+
+    result = 0
+    sess = tf.Session()
+    xx = sess.run(result)
+    print(result)
+
+
+    c= tf.constant([1.0, 2, 0], name='a')
+    d = tf.constant([2.0, 3, 0], name='b')
+    result = c + d
+    print result
+    # 运行结果为： Tensor("add:0", shape=(3,), dtype=float32) 。并未如ppt所说给出运行结果
