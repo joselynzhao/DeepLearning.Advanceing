45ef5cc12dbf9163ea83dab9bffb87f0d7224378
zhaojing1995 <1326549593@qq.com>
2019-05-15 13:10:01 +0800

    DL-3: work  all over

--- DL-3/work/main2.py
+++ DL-3/work/main2.py
@@ -10,6 +10,8 @@
 @DES:
 '''
 
+import matplotlib.pyplot as plt # plt 用于显示图片
+import matplotlib
 import tensorflow as tf
 old_v = tf.logging.get_verbosity()
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -27,7 +29,7 @@ if __name__ =="__main__":
                 scope.reuse_variables()
             W = tf.Variable(tf.zeros([392,10]),name='w')
             y = tf.matmul(x,W)
-            return y
+            return W,y
 
     input1 = tf.placeholder(dtype='float',shape=[None,392])
     input2 = tf.placeholder(dtype='float',shape=[None,392])
@@ -35,11 +37,13 @@ if __name__ =="__main__":
     # x = tf.placeholder(dtype='float',shape=[None,784])
     # w = tf.Variable(tf.zeros([784,10]))
     b = tf.Variable(tf.zeros([10]))
-    y = tf.nn.softmax(X_W(input1)+X_W(input2,True)+b)
+    _,y1 = X_W(input1)
+    weight,y2 = X_W(input2,True)
+    y = tf.nn.softmax(y1+y2+b)
     y_ = tf.placeholder(dtype='float',shape=[None,10])
 
     cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-    train_step = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate=0.005).minimize(cross_entropy)
     #准确率
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
@@ -47,15 +51,28 @@ if __name__ =="__main__":
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
-    step = 5000
+    step = 10000
     batch_size = 64
     # loss_list = []
     for i in range(step):
-        batch_xs,batch_ys = mnist.train.next_batch(batch_size) #shape: (100, 784) (100, 10) _,
-        _,loss= sess.run([train_step,cross_entropy],feed_dict={input1:batch_xs[:,0:392],input2:batch_xs[:,392:784],y_:batch_ys})
-        # loss_list.append(loss)
+        batch_xs,batch_ys = mnist.train.next_batch(batch_size)
+        _,loss,w= sess.run([train_step,cross_entropy,weight],feed_dict={input1:batch_xs[:,0:392],input2:batch_xs[:,392:784],y_:batch_ys})
         if i % 500 ==1:
             acc = sess.run(accuracy,feed_dict={input1:mnist.test.images[:,0:392],input2:mnist.test.images[:,392:784],y_:mnist.test.labels})
             print("%5d: accuracy is: %4f" % (i, acc))
 
     print('[accuracy,loss]:',sess.run([accuracy,cross_entropy],feed_dict={input1:mnist.test.images[:, 0:392],input2:mnist.test.images[:, 392:784],y_:mnist.test.labels}))
+    w = np.array(w)
+    font1 = {'family': 'Times New Roman',
+             'weight': 'normal',
+             'size': 7,}
+    matplotlib.rc('font', **font1)
+    plt.figure()
+    for i in range(10):
+        weight = w[:,i]
+        weight = np.reshape(weight,[14,28])
+        plt.subplot(5,2,i)
+        plt.title(i)
+        plt.imshow(weight)
+    plt.savefig('./save/weight.png')
+    plt.show()
