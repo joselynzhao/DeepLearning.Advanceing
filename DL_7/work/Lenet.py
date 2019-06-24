8c3b0d1067286b1436c25c0b42484ca7b2d316b0
zhaojing1995 <1326549593@qq.com>
2019-06-24 16:51:52 +0800

    DL_7：20%

---
+++ DL_7/work/Lenet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn Zhao
+@CONTACT:zhaojing17@foxmail.com
+@HOME_PAGE:joselynzhao.top
+@SOFTWERE:PyCharm
+@FILE:Lenet.py
+@TIME:2019/6/24 16:51
+@DES:
+'''
